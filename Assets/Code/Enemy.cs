2900943f9e8c0da576c5eedef29eb28b2a67581d
Renan Barbosa <renan_barbosa91@hotmail.com>
2020-07-24 17:47:24 -0300

    update
    adicionado particulas ao matar um monstro e ele morre ao colidir ou com o pássaro ou com a caixa vindo de cima.

---
+++ Assets/Code/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _CloudParticlePrefab;
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Bird bird = collision.collider.GetComponent<Bird>();
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+
+        if (bird != null)
+        {
+            Instantiate(_CloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+
+        if (enemy != null)
+        {
+            return;
+        }
+        
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            Instantiate(_CloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        } 
+    }
+}
