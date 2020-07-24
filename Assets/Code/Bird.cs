63c19d04128fe0d3e9ddc83c9a6820d178430be4
Renan Barbosa <renan_barbosa91@hotmail.com>
2020-07-24 13:24:18 -0300

    pasta assets enviada

---
+++ Assets/Code/Bird.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Vector3 initialPosition;
+
+    private void Awake() {
+        initialPosition = transform.position;
+    }
+
+    private void OnMouseDown(){
+        GetComponent<SpriteRenderer>().color = Color.red;
+        
+    }
+    
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+        Vector2 directionToInitialPosition = initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition);
+    }
+
+    private void OnMouseDrag() {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+}
